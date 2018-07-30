18d99377f218012df0a876c81088ed17c15f1618
ttshivhula <pboyfunclub@gmail.com>
2018-07-30 20:04:18 +0200

    Commenting my code

--- Vertex.java
+++ Vertex.java
@@ -3,6 +3,15 @@ package bfs;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * The Vertex class is used as a data structure all the necesary variables
+ * together with their setters and getters are defined in this class
+ * name is the room name, visited is a boolean we use to check if the room
+ * is visited, neigbours is a list of type Vertex because all the neigbours have
+ * all the properties of the Vertex class. we can define neigbours as rooms 
+ * connected to a room. 
+*/
+
 public class Vertex {
 	private String name;
 	private boolean visited;
